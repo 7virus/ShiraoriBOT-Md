bd113eb8d4b49d3f72272fad52b3eb677fbe5e90
7virus <virus7suriv@gmail.com>
2025-01-07 05:07:00 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 
-global.owner = ['6281351047727'] // Put your number here
+global.owner = ['254112386921'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
@@ -29,10 +29,10 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
 global.packname = 'ShiraoriBOT Multi device'
-global.author = 'Ilmanhdyt'
+global.author = '7virus'
 global.namabot = 'ShiraoriBOT'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.wm = '© ShiraoriBOT-Md • Ilmanhdyt'
+global.wm = '© 7virusBOT-Md '
 global.media = 'https://telegra.ph/file/5d45a3d1e12726d98c5e4.jpg'
 
 global.wait = '_*tunggu sedang di proses...*_'
@@ -43,7 +43,7 @@ global.urlnya = "https://www.instagram.com/ilmanhdyt_"
 
 //============= callButtons =============//
 global.dtc = 'ᴄᴀʟʟ ᴏᴡɴᴇʀ'
-global.phn = '+62 813-5104-7727'
+global.phn = '+254112386921'
 
 //============= Games ================//
 global.benar = '_*Benar✅*_'
